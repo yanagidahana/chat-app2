ef9c54ff8dab258faf2eaf86cc78aa88ead26928
yanagidahana <hana878787zzz@gmail.com>
2020-10-25 15:30:17 +0900

    プルダウンの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,8 @@ class User < ApplicationRecord
          has_many :room_users
          has_many :rooms, through: :room_users
          validates :name, presence: true
+
+         has_many :room_users
+         has_many :rooms, through: :room_users
          
 end
